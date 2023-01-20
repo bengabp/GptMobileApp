f1d01ccdbbf8fa177c0299e1f47e357dc8a32434
Onyebuchi Benedict <84660669+Bengab-coder@users.noreply.github.com>
2023-01-20 04:11:33 +0100

    building chat ui

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,8 +1,10 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:ai4study/screens/prechat.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 class ChatScreen extends StatefulWidget {
-
   @override
   State<ChatScreen> createState() => _ChatScreenState();
 }
@@ -13,10 +15,67 @@ class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Ai4Study")),
-      body: Column(
-        children: [Text("Chat Screen")],
+      appBar: AppBar(
+        title: Text("Ai4Study"),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading:IconButton(onPressed: ()=>{}, icon:Icon(Icons.menu)),
+        toolbarHeight: 70,
       ),
+      body: Stack(
+        children: <Widget>[
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Container(
+              padding: EdgeInsets.only(left: 10, bottom: 10, top: 10),
+              height: 60,
+              width: double.infinity,
+              color: Colors.white,
+              child: Row(
+                children: <Widget>[
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      height: 30,
+                      width: 30,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: Icon(
+                        Icons.camera_alt_outlined,
+                        color:Colors.orange,
+                        size: 20,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Expanded(
+                    child: CupertinoTextField(
+                      placeholder: "Write message...",
+                      padding:EdgeInsets.all(10)
+                    ),
+                  ),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  FloatingActionButton(
+                    onPressed: () {},
+                    child: Icon(
+                      Icons.send,
+                      color: Colors.white,
+                      size: 18,
+                    ),
+                    elevation: 0,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+      
     );
   }
 }
