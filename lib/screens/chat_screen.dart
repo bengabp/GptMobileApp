2a003dac5b2abf87fa690a76b53028b29c4284a3
Onyebuchi Benedict <84660669+Bengab-coder@users.noreply.github.com>
2023-01-22 00:42:01 +0100

    Integrated chatbot

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:ai4study/screens/chat_widget.dart';
@@ -9,6 +10,7 @@ import 'package:flutter/cupertino.dart';
 import './chat_widget.dart';
 import 'package:flutter_sound/flutter_sound.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:http/http.dart' as http;
 
 class ChatScreen extends StatefulWidget {
   @override
@@ -19,17 +21,22 @@ class _ChatScreenState extends State<ChatScreen>
     with SingleTickerProviderStateMixin {
   bool is_first_setup = true;
   bool is_recording = false;
-  List<ChatMessage> chat_messages = [
-    ChatMessage("Hi", true),
-    ChatMessage("How have you been?", false),
-    ChatMessage("Hey Kriss, I am doing fine dude. wbu?", false)
-  ];
+  List<ChatMessage> chat_messages = [];
   String chat_text_message = "";
   ScrollController _scrollController = ScrollController();
   TextEditingController _messageController = TextEditingController();
 
   FlutterSoundRecorder recorder = FlutterSoundRecorder();
   bool isRecorderReady = false;
+  var fakePath;
+
+  SnackBar _createSnackBar(String text) {
+    return SnackBar(
+        content: Text(text),
+        dismissDirection: DismissDirection.horizontal,
+        behavior: SnackBarBehavior.floating,
+        margin: EdgeInsets.only(bottom: 80, left: 20, right: 20));
+  }
 
   @override
   void initState() {
@@ -62,7 +69,32 @@ class _ChatScreenState extends State<ChatScreen>
     if (!isRecorderReady) return;
     final path = await recorder.stopRecorder();
     final audioFile = File(path!);
+    fakePath = audioFile.absolute;
     print("Recorded audio: $audioFile");
+  }
+
+  Future _makeNetworkRequest() async {
+    print("making network request");
+    final response =
+        await http.get(Uri.parse('http://139.162.220.59:8000/testapi'));
+    var decodedJson = jsonDecode(utf8.decode(response.bodyBytes)) as Map;
+  }
+
+  Future _getBotResponse(String userMessage) async {
+    final response = await http.get(
+        Uri.parse("http://139.162.220.59:8000/chat/text?text=$userMessage"));
+    var decodedJson = jsonDecode(utf8.decode(response.bodyBytes)) as Map;
+    String textResponse = decodedJson["botReply"];
+    return textResponse;
+  }
+
+  _scrollToBottom() {
+    try{
+      _scrollController.animateTo(_scrollController.position.maxScrollExtent + 50,
+          duration: Duration(milliseconds: 500), curve: Curves.easeOut);
+    } on AssertionError {
+
+    }
   }
 
   @override
@@ -82,22 +114,30 @@ class _ChatScreenState extends State<ChatScreen>
               overscroll.disallowIndicator();
               return true;
             },
-            child: SingleChildScrollView(
+            child: chat_messages.length <= 0
+                ? Center(
+                    child: Text(
+                      "No messages yet",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                    ),
+                  )
+                :  SingleChildScrollView(
               physics: ScrollPhysics(),
               controller: _scrollController,
               padding: EdgeInsets.only(bottom: 100),
               child: Container(
                 padding: EdgeInsets.only(left: 10, right: 10),
                 child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: List.generate(chat_messages.length, (index) {
-                      String messageText = chat_messages[index].text;
-                      bool isMine = chat_messages[index].is_my_message;
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: List.generate(chat_messages.length, (index) {
+                          String messageText = chat_messages[index].text;
+                          bool isMine = chat_messages[index].is_my_message;
 
-                      return isMine
-                          ? UserMessage(messageText, DateTime.now())
-                          : BotMessage(messageText, DateTime.now());
-                    })),
+                          return isMine
+                              ? UserMessage(messageText, DateTime.now())
+                              : BotMessage(messageText, DateTime.now());
+                        })),
               ),
             ),
           ),
@@ -148,8 +188,13 @@ class _ChatScreenState extends State<ChatScreen>
                           is_recording = !is_recording;
                           if (is_recording) {
                             record();
+                            ScaffoldMessenger.of(context)
+                                .showSnackBar(_createSnackBar("Recording..."));
                           } else {
                             stop();
+                            ScaffoldMessenger.of(context).showSnackBar(
+                                _createSnackBar(
+                                    "Stopped Recording...PATH:$fakePath"));
                           }
                         });
                       },
@@ -158,21 +203,28 @@ class _ChatScreenState extends State<ChatScreen>
                       elevation: 0),
                   FloatingActionButton(
                     heroTag: "SendButton",
-                    onPressed: () {
-                      if (chat_text_message.length > 0) {
+                    onPressed: () async {
+                      String userMessage = _messageController.text.trim();
+                      if (userMessage.length > 0) {
                         // Send message to bot
                         ChatMessage new_chat_message =
-                            ChatMessage(chat_text_message, true);
+                            ChatMessage(userMessage, true);
                         setState(() {
                           chat_messages.add(new_chat_message);
                           _messageController.clear();
                         });
-                        _scrollController.animateTo(
-                            _scrollController.position.maxScrollExtent + 100,
-                            duration: Duration(milliseconds: 1000),
-                            curve: Curves.easeOut);
+                        _scrollToBottom();
+
+                        // Get Bot Response
+                        String botReply = await _getBotResponse(userMessage);
+                        ChatMessage new_bot_message =
+                            ChatMessage(botReply, false);
+                        setState(() {
+                          chat_messages.add(new_bot_message);
+                          userMessage = "";
+                        });
+                        _scrollToBottom();
                       }
-                      print(_scrollController.offset);
                     },
                     child: Icon(
                       Icons.send,
